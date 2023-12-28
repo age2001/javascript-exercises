5f5f6221f48f885102fe8322af2632fb2f85116a
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-12-28 15:36:19 -0600

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (letter of string) {
+        reversedString = letter + reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
