2268f278483c6f5d9ef07c8928ec8dbf0d9fb993
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-12-28 16:40:50 -0600

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numList, ...args) {
+    let valInList = false;
+    let finalNumList = [];
+    for (num of numList) {
+        for (delVal of args) {
+            if (num === delVal) {valInList = true;}
+        }
+        if (!valInList) {finalNumList.push(num);}
+        valInList = false;
+    }
+    return finalNumList;
 };
 
 // Do not edit below this line
