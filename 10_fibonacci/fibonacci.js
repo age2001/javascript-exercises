0bd7c88c45d91ba61364fde5fe57621da3ada19a
Alan Enriquez <alan.enriquez2001@gmail.com>
2024-01-16 22:19:33 -0600

    Finish 10 fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n, n0 = 1, n1 = 1, count = 2) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
+    if (count === n) {return n1;}
+    return fibonacci(n, n1, n0+n1, ++count);
 };
 
 // Do not edit below this line
