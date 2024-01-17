c5192d94d86f5824f0b86dedcf872a15afb8ae52
Alan Enriquez <alan.enriquez2001@gmail.com>
2024-01-16 22:02:50 -0600

    Finish assignment 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    if (str.split("").reverse().join("") == str) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
