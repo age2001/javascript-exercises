5a2890d1e82b3ca8de93d7990bbbc622697eeece
Alan Enriquez <alan.enriquez2001@gmail.com>
2024-01-16 23:05:02 -0600

    Finish 12 findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let person = people.reduce((p1, p2) => {
+        if ("yearOfDeath" in p1 === false) {
+            let date = new Date();
+            p1.yearOfDeath = date.getFullYear();
+        }
+        if ("yearOfDeath" in p2 === false) {
+            let date = new Date();
+            p2.yearOfDeath = date.getFullYear();
+        }
+        if (p1.yearOfDeath - p1.yearOfBirth > p2.yearOfDeath - p2.yearOfBirth) {
+            return p1;
+        }
+        return p2;
+    });
+    return person;
 };
 
 // Do not edit below this line
